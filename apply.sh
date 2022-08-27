6653c8e1e1187508e77ee9ac714e669328d2a272
Nanhumly <1607616422@qq.com>
2022-08-27 03:09:11 +0000

    Add patches for KaleidoscopeOS

--- apply.sh
+++ apply.sh
@@ -18,9 +18,9 @@ for project in $(cd $patches/patches/$tree; echo *);do
 done
 }
 
-if [ $1 = arrow ];then
+if [ $1 = kscope ];then
 echo ""
-echo "Adapt ArrowOS to Sagit"
+echo "Adapt KaleidoscopeOS to Sagit"
 echo "Executing in 5 seconds - CTRL-C to exit"
 echo ""
 fi
